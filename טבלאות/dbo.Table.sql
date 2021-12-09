8661df4342517227c2abf9279ba9e812086f3ab0
Tahel <tahelqq@gmail.com>
2021-12-09 19:09:19 +0200

    2

---
+++ טבלאות/dbo.Table.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Users](
+	[User_Code] [int] IDENTITY(1,1) NOT NULL,
+	[User_Name] VARCHAR(50) NULL,
+	[Password] VARCHAR(50) NULL
+)
---
+++ טבלאות/dbo.Table_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products](
+	[Product_Code] [int] IDENTITY(1,1) NOT NULL,
+	[product_name] [nvarchar](50) NULL,
+	[category] [nvarchar](50) NULL, 
+    [Location_Code] NCHAR(10) NULL, 
+    [Price] NCHAR(10) NULL
+
+)
