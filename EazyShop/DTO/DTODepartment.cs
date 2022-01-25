a0de5a21c6b582332e19dc9dcd9f3f0fd828dc25
tahell <95773940+tahell@users.noreply.github.com>
2022-01-25 09:54:16 +0200

    25/01/22

---
+++ EazyShop/DTO/DTODepartment.cs
@@ -0,0 +1,51 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DTODepartment
+    {
+        public int Class_Code { get; set; }
+        public string Class_Name { get; set; }
+
+        public DTODepartment()
+        {
+
+        }
+        public DTODepartment(Department d)
+        {
+            this.Class_Code = d.Class_Code;
+            this.Class_Name = d.Class_Name;
+
+        }
+        public Department FromDTOToTable(DTODepartment dt)
+        {
+            Department d = new Department();
+            d.Class_Code = dt.Class_Code;
+            d.Class_Name = dt.Class_Name;
+            return d;
+        }
+
+        public static List<DTODepartment> CreatDtoList(List<Department> d)
+        {
+            List<DTODepartment> dtolist = new List<DTODepartment>();
+            foreach(var c in d)
+            {
+                DTODepartment dTO = new DTODepartment(c);
+                dtolist.Add(dTO);
+            }
+            return dtolist;
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ EazyShop/DTO/DTOUser.cs
@@ -0,0 +1,49 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+     public class DTOUser
+    {
+        public int User_Code { get; set; }
+        public string User_Name { get; set; }
+        public string Password { get; set; }
+
+        public DTOUser()
+        {
+
+        }
+
+        public DTOUser(User u)
+        {
+            this.User_Code = u.User_Code;
+            this.User_Name = u.User_Name;
+            this.Password = u.Password;
+        }
+        public User FromDTOToTable(DTOUser u)
+        {
+            User us = new User();
+            us.User_Code = u.User_Code;
+            us.User_Name = u.User_Name;
+            us.Password = u.Password;
+            return us;
+        }
+
+        public static List<DTOUser> CreatDtoList(List<User> d)
+        {
+            List<DTOUser> dtolist = new List<DTOUser>();
+            foreach (var c in d)
+            {
+                DTOUser dTO = new DTOUser(c);
+                dtolist.Add(dTO);
+            }
+            return dtolist;
+
+        }
+
+    }
+}
